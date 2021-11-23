b53a25b0c3f93636ab4d908ccadbcadb9be22880
Raitis-Silins <raiti.silins@gmail.com>
2021-11-23 23:19:02 +0200

    Exercise_1 complete

---
+++ csharp-basics/exercises/Arithmetic/Exercise1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(checkIntegers(6, 21));
+        }
+        public static bool checkIntegers(int num1, int num2)
+        {
+            return num1 == 15 || num2 == 15 || num1 + num2 == 15 || num1 - num2 == 15 || num2 - num1 == 15;
+        }
+    }
+}
