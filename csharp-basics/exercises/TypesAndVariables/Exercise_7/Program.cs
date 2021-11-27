0a979f12dfb3d1f66d2e8f85536f30b1a14bd561
Raitis-Silins <raiti.silins@gmail.com>
2021-11-27 17:29:44 +0200

    Exercise_7 complete

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise_7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a string and get the number of the uppercase letters in the string");
+            string userInput = Console.ReadLine().ToString();
+
+            var add = 0;
+            foreach (var upperLetter in userInput)
+            {
+                if (char.IsUpper(upperLetter))
+                {
+                    add++;
+                }
+            }
+
+            Console.WriteLine($"There are {add} uppercase letters");
+        }
+    }
+}
