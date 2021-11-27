44bfa4132c355fe33a434a3ebe52264210b3ef30
Raitis-Silins <raiti.silins@gmail.com>
2021-11-27 13:34:13 +0200

    Exercise_4 complete

---
+++ csharp-basics/exercises/TypesAndVariables/Exercise_4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Raitis Siliņš";
+            int birthYear = 1993;
+            Console.WriteLine("My name is {0} and I was born in {1}.", name, birthYear);
+        }
+    }
+}
