f2a4e5422cb0c96e4e543118568f23a4b35e7f3d
Raitis-Silins <raiti.silins@gmail.com>
2021-11-27 19:41:45 +0200

    Exercise_1 complete

--- csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
+++ csharp-basics/exercises/FlowOfControl/FlowControl/LargestNumber/Program.cs
@@ -8,20 +8,26 @@ namespace LargestNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Input the 1st number: ");
-            var input1 = Console.ReadLine();
+            var input1 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Input the 2nd number: ");
-            var input2 = Console.ReadLine();
+            var input2 = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Input the 3rd number: ");
-            var input3 = Console.ReadLine();
-        
-            /*
-            todo - expected output:
-            Input the 1st number: 25
-            Input the 2nd number: 78
-            Input the 3rd number: 87
-             */
+            var input3 = int.Parse(Console.ReadLine());
+
+            if (input1 > input2 && input1 > input3)
+            {
+                Console.WriteLine($"Number {input1} is the largest");
+            }
+            else if (input2 > input1 && input2 > input3)
+            {
+                Console.WriteLine($"Number {input2} is the largest");
+            }
+            else if (input3 > input1 && input3 > input2)
+            {
+                Console.WriteLine($"Number {input3} is the largest");
+            }
         }
     }
 }
