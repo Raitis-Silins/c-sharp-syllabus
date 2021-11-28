297df8de4c6222dcb50bc6691423cf95020804b9
Raitis-Silins <raiti.silins@gmail.com>
2021-11-29 00:06:42 +0200

    Exercise4 fix.

--- csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
+++ csharp-basics/exercises/Arrays/Arrays/Exercise4/Program.cs
@@ -8,21 +8,18 @@ namespace Exercise4
 {
     class Program
     {
-        //TODO: Write a C# program to test if an array contains a specific value.
         private static void Main(string[] args)
         {
             int[] myArray =
             {
                 1789, 2035, 1899, 1456, 2013,
                 1458, 2458, 1254, 1472, 2365,
-                1456, 2265, 1457, 2456
+                1456, 2265, 1457, 2456,
             };
 
-            int index = Array.IndexOf(myArray, 1254);
-            if (index > -1)
-            {
-                Console.WriteLine($"Contains!");
-            }
+            var index = Array.FindIndex(myArray, x => x.Equals(1245));
+
+            Console.WriteLine(index >= 0 ? "Contains" : "Does not contains");
         }
     }
 }
