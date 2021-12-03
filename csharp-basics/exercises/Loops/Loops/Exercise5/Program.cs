f08cbdb839f2cc1605c4d94398cf44707dae1a1b
Raitis-Silins <raiti.silins@gmail.com>
2021-12-03 21:44:32 +0200

    Exercise5 complete

---
+++ csharp-basics/exercises/Loops/Loops/Exercise5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first word:");
+            string firstWord = Console.ReadLine();
+            Console.WriteLine("Enter second word");
+            string secondWord = Console.ReadLine();
+
+            var lengthBetween = 30 - firstWord.Length - secondWord.Length;
+            Console.WriteLine(firstWord +String.Concat(Enumerable.Repeat(".", lengthBetween))+ secondWord );
+        }
+    }
+}
