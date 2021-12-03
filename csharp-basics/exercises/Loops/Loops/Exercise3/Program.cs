2ad43f3ebd508dd7e5fff93d55767b83b27e561d
Raitis-Silins <raiti.silins@gmail.com>
2021-12-03 21:13:16 +0200

    Exercise3 complete

---
+++ csharp-basics/exercises/Loops/Loops/Exercise3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[20];
+
+            Random random = new Random();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(0, 20);
+            }
+
+            Console.WriteLine("Which number position out of 20 random numbers you want to know");
+            var userInput = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your number is {0}", array[userInput]);
+        }
+    }
+}
