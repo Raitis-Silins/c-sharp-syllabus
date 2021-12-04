909fd7d492e31056099b28ec6d80923837a55155
Raitis-Silins <raiti.silins@gmail.com>
2021-12-04 21:02:29 +0200

    Exercise7 complete

---
+++ csharp-basics/exercises/Loops/Loops/Exercise7/Piglet .cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Piglet
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Welcome to Piglet!");
+            Random random = new Random();
+
+            var currentScore = 0;
+
+            while (true)
+            {
+                var currentRoll = random.Next(1, 7);
+
+                if (currentRoll == 1)
+                {
+                    currentScore = 0;
+                    break;
+                }
+
+                currentScore += currentRoll;
+                Console.WriteLine("You rolled {0}", currentRoll);
+                Console.WriteLine("Roll again? y/n");
+
+                var userInput = Console.ReadKey().KeyChar;
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine();
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    break;
+                }
+            }
+
+            Console.WriteLine($"Your score was {currentScore} ");
+        }
+    }
+}
