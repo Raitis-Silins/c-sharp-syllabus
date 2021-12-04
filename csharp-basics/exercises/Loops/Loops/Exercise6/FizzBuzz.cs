a195725d82056f0f0c69dc02b056c9a3ded75a50
Raitis-Silins <raiti.silins@gmail.com>
2021-12-04 17:15:33 +0200

    Exercise6 complete

---
+++ csharp-basics/exercises/Loops/Loops/Exercise6/FizzBuzz.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter an integer, that prints all of the numbers.");
+            int userInput = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= userInput; i++)
+            {
+                 if (i % 3 == 0 && i % 5 == 0)
+                 {
+                     Console.Write("FizzBuzz ");
+                 } 
+                 else if (i % 5 == 0)
+                 {
+                     Console.Write("Buzz ");
+                 }
+                 else if (i % 3 == 0)
+                 {
+                     Console.Write("Fizz ");
+                 }
+                 else
+                 {
+                     Console.Write(i + " ");
+                 }
+
+                 if (i % 20 == 0)
+                 {
+                     Console.WriteLine();
+                 }
+            }
+        }
+    }
+}
