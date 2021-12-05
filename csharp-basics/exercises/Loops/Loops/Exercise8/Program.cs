b597ae6729af445b1a4b38f51c83f83d8f80ee42
Raitis-Silins <raiti.silins@gmail.com>
2021-12-05 22:13:52 +0200

    Exercise8 complete

--- csharp-basics/exercises/Loops/Loops/Exercise8/Program.cs
+++ csharp-basics/exercises/Loops/Loops/Exercise8/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 namespace Exercise8
 {
@@ -7,7 +6,25 @@ namespace Exercise8
     {
         static void Main(string[] args)
         {
-            
+            int userInput = int.Parse(Console.ReadLine());
+
+            int x = -4 + userInput;
+            var oneSide = (((userInput * 3) + x));
+
+            Console.Write(new string('/', oneSide));
+            Console.Write(new string('\\', oneSide));
+            Console.WriteLine();
+
+            for (int i = 1; i < userInput - 1; i++)
+            {
+                Console.Write(new string('/', oneSide - (i * 4)));
+                Console.Write(new string('*', (4 * i) * 2));
+                Console.Write(new string('\\', oneSide - (i * 4)));
+                Console.WriteLine();
+            }
+
+            Console.Write(new string('*', (oneSide * 2)));
+
         }
     }
 }
