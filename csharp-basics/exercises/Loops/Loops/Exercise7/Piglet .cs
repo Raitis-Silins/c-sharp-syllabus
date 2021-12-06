78b18db308ef86760509edbb364b47d58e7c4334
Raitis-Silins <raiti.silins@gmail.com>
2021-12-06 23:03:43 +0200

    Changes made

--- csharp-basics/exercises/Loops/Loops/Exercise7/Piglet .cs
+++ csharp-basics/exercises/Loops/Loops/Exercise7/Piglet .cs
@@ -29,7 +29,6 @@ namespace Exercise7
 
                 if (userInput == 'y')
                 {
-                    Console.WriteLine();
                     continue;
                 }
                 else
