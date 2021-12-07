1aaef5ec7f559fbaacdfa099d1c8ea1882995996
Raitis-Silins <raiti.silins@gmail.com>
2021-12-07 03:05:02 +0200

    Exercise5 complete

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5/Date .cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Date
+    {
+        private int _month { get; set; }
+
+        private int _day { get; set; }
+
+        private int _year { get; set; }
+
+        public Date(int month, int day, int year)
+        {
+            _month = month;
+            _day = day;
+            _year = year;
+        }
+
+        public void DisplayDate()
+        {
+            Console.WriteLine($"{_month}/{_day}/{_year}");
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise5
+{
+    class Program
+    {
+        static void Main()
+        {
+            Date DateTest = new Date(12, 01, 2017);
+
+            DateTest.DisplayDate();
+        }
+    }
+}
