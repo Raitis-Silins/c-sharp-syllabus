d4f5257fbd2a94d054cb581ebe9ea8906cae0f6f
Raitis-Silins <raiti.silins@gmail.com>
2021-12-07 02:40:01 +0200

    Exercise1 complete

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise1/Product .cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Product
+    {
+        private double _price;
+        private int _amount;
+        private string _name;
+
+        public Product(string name, double priceAtStart, int amountAtStart)
+        {
+            _price = priceAtStart;
+            _amount = amountAtStart;
+            _name = name;
+        }
+
+        public void PrintProduct()
+        {
+            Console.WriteLine($"{_name}, price {_price} EUR, amount {_amount} units");
+        }
+
+        public void ChangeQuantity(int quantity)
+        {
+            _amount = quantity;
+        }
+
+        public void ChangePrice(double price)
+        {
+            _price = price;
+        }
+    }
+}
