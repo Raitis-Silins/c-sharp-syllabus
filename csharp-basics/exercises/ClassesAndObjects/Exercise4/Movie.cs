d2bee5395081636443ed73579fc7b593b5ead90d
Raitis-Silins <raiti.silins@gmail.com>
2021-12-08 01:49:31 +0200

    Exercise4 complete

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise4/Movie.cs
@@ -0,0 +1,33 @@
+﻿namespace Exercise4
+{
+    public class Movie
+    {
+        public Movie(string title, string studio, string rating)
+        {
+            _title = title;
+            _studio = studio;
+            Rating = rating;
+        }
+
+        public Movie(string title, string studio)
+        {
+            _title = title;
+            _studio = studio;
+            Rating = "PG";
+        }
+
+        private string _title { get; set; }
+        private string _studio { get; set; }
+        public string Rating { get; private set; }
+
+        public string Display()
+        {
+            return $"{_title}, {_studio}, {Rating}";
+        }
+
+        public override string ToString()
+        {
+            return $"{_title}, {_studio}, {Rating}";
+        }
+    }
+}
