44a7de94ae462ad66c13089db7f559702885b8cf
Raitis-Silins <raiti.silins@gmail.com>
2021-12-08 04:12:54 +0200

    Exercise7 complete

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise7/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Exercise7
+{
+    class Program
+    {
+        static void Main()
+        {
+            DogTest test = new DogTest();
+            Console.WriteLine(test.dog8.FathersName());
+            Console.WriteLine(test.dog3.FathersName());
+
+            Console.WriteLine(test.dog2.HasSameMotherAs(test.dog8));
+        }
+    }
+}
