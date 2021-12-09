279cdaa5b5f31750946d4d88e2d59d0e18def347
Raitis-Silins <raiti.silins@gmail.com>
2021-12-09 04:19:12 +0200

    Exercise8 complete

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal _startBalance;
+            decimal _annualInterestRate;
+            int _months;
+            decimal _depositAmount;
+            decimal _withdrawAmount;
+
+            Console.Write("How much money is in the account?:  ");
+            _startBalance = decimal.Parse(Console.ReadLine());
+            Console.Write("Enter the annual interest rate:  ");
+            _annualInterestRate = decimal.Parse(Console.ReadLine());
+            Console.Write("How long has the account been opened?:  ");
+            _months = int.Parse(Console.ReadLine());
+
+            SavingsAccount test = new SavingsAccount(startBalance: _startBalance, annualInterestRate: _annualInterestRate);
+
+            for (var i = 0; i < _months; i++)
+            {
+                Console.Write($"Enter amount deposited for month: {i + 1}  "); 
+                _depositAmount = decimal.Parse(Console.ReadLine());
+
+                test.Deposit(_depositAmount); 
+
+                Console.Write($"Enter amount withdrawn for month: {i + 1}  ");
+                _withdrawAmount = decimal.Parse(Console.ReadLine());
+
+                test.Withdraw(_withdrawAmount); 
+                test.AcquireMonthlyInterest();
+            }
+
+            DisplayData(test);
+        }
+
+        public static void DisplayData(SavingsAccount test)
+        {
+            decimal _balance = Math.Round(test.GetBalance() * (decimal)100.0) / (decimal)100.0; 
+            decimal _totalInterest = Math.Round(test.GetTotalnterest() * (decimal)100.0) / (decimal)100.0;
+
+            Console.WriteLine();
+            Console.WriteLine($"Total deposited: ${test.GetTotalDeposits()}");
+            Console.WriteLine($"Total withdrawn: ${test.GetTotalWithdraws()}");
+            Console.WriteLine($"Interest earned: ${_totalInterest}");
+            Console.WriteLine($"Ending balance: ${_balance}"); 
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise8/SavingsAccount .cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace Exercise8
+{
+    class SavingsAccount
+    {
+        private decimal _balance;
+        private decimal _annualInterestRate;
+        private decimal _totalDeposits;
+        private decimal _totalWithdraws;
+        private decimal _totalInterest;
+
+        public SavingsAccount(decimal startBalance, decimal annualInterestRate)
+        {
+            _balance = startBalance;
+            _annualInterestRate = annualInterestRate;
+        }
+
+        public void Deposit(decimal amount)
+        {
+            _balance += amount;
+            _totalDeposits += amount;
+        }
+
+        public void Withdraw(decimal amount)
+        {
+            _balance -= amount;
+            _totalWithdraws += amount;
+        }
+
+        public void AcquireMonthlyInterest()
+        {
+            decimal monthlyInterest = _balance * GetMonthlyInterestRate();
+            _totalInterest += monthlyInterest;
+            _balance += monthlyInterest;
+        }
+
+        public decimal GetMonthlyInterestRate()
+        {
+            return _annualInterestRate / 12;
+        }
+
+        public decimal GetBalance()
+        {
+            return _balance;
+        }
+
+        public decimal GetTotalDeposits()
+        {
+            return _totalDeposits;
+        }
+
+        public decimal GetTotalWithdraws()
+        {
+            return _totalWithdraws;
+        }
+
+        public decimal GetTotalnterest()
+        {
+            return _totalInterest;
+        }
+    }
+}
