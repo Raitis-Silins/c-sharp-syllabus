5215a6712126f6e42c38aacacede5a461a2aa1fe
Raitis-Silins <raiti.silins@gmail.com>
2021-12-10 03:34:23 +0200

    Exercise9 complete

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise9/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise9
+{
+    class Point
+    {
+        public int x;
+        public int y;
+        
+        public Point(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+    }
+}
