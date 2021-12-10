dab8b992c14768eac0e72295db5093bb7162db1e
Raitis-Silins <raiti.silins@gmail.com>
2021-12-10 02:38:02 +0200

    Exercise3 complete

---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Odometer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Odometer
+    {
+        public double Mileage;
+        public FuelGauge ActualLiters;
+
+        public Odometer(double mileage, FuelGauge actualLiters)
+        {
+            Mileage = mileage;
+            ActualLiters = actualLiters;
+        }
+
+        public void DisplayMileage()
+        {
+            Console.WriteLine(Mileage);
+        }
+
+        public void Increment()
+        {
+            if (Mileage <= 999999)
+            {
+                Mileage++;
+            }
+            else
+            {
+                Mileage = 0;
+            }
+        }
+
+        public void Decrement()
+        {
+            if (Mileage % 10 == 0)
+            {
+                ActualLiters.DecrementFuel();
+            }
+        }
+    }
+}
---
+++ csharp-basics/exercises/ClassesAndObjects/Exercise3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main()
+        {
+            FuelGauge fuel = new FuelGauge(0);
+            Odometer odo = new Odometer(0, fuel);
+
+            Console.Write("Fill the tank ");
+            var userInput = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < userInput; i++)
+            {
+                fuel.IncrementFuel();
+                fuel.ShowLiters();
+            }
+
+            while (fuel.ActualLiters != 0)
+            {
+                odo.Increment();
+                Console.Write("Fuel tank now: ");
+                fuel.ShowLiters();
+                Console.WriteLine();
+                odo.Decrement();
+                Console.Write("Km now: ");
+                odo.DisplayMileage();
+            }
+
+            Console.WriteLine("Out of fuel");
+        }
+    }
+}
