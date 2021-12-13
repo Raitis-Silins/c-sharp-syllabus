4078d55b6d506400e4d20435c150e4b0d3a93667
Raitis-Silins <raiti.silins@gmail.com>
2021-12-14 01:52:06 +0200

    ListExercise3 complete

--- csharp-basics/exercises/Collections/ListExercise3/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise3/Program.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ListExercise3
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //TODO: Write a C# program to insert an element into the array list at the first position.
-
             var colors = new List<string>();
             colors.Add("Red");
             colors.Add("Green");
@@ -20,10 +14,9 @@ namespace ListExercise3
             colors.Add("Black");
 
             Console.WriteLine(string.Join(",", colors));
+            colors.Insert(0, "Blue");
+            colors.Insert(2, "Pink");
 
-            //TODO: Add new color at the first and third position of the list
-
-            //Print the list
             Console.WriteLine(string.Join(",", colors));
         }
     }
