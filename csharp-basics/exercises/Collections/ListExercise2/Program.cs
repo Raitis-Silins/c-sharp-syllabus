e5d20d693c00525470b3276a649c9a5a9b7b9237
Raitis-Silins <raiti.silins@gmail.com>
2021-12-14 01:45:48 +0200

    ListExercise2 complete

--- csharp-basics/exercises/Collections/ListExercise2/Program.cs
+++ csharp-basics/exercises/Collections/ListExercise2/Program.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace ListExercise2
 {
     class Program
     {
         private static void Main(string[] args)
         {
-            //TODO: Write a C# program to iterate through all elements in a list.
-
             var colors = new List<string>();
             colors.Add("Red");
             colors.Add("Green");
@@ -19,13 +13,10 @@ namespace ListExercise2
             colors.Add("White");
             colors.Add("Black");
 
-            /*
-            fixme
-            for (...) 
+            foreach (var element in colors)
             {
-              Console.WriteLine(element);
+                Console.WriteLine(element);
             }
-            */
         }
     }
 }
