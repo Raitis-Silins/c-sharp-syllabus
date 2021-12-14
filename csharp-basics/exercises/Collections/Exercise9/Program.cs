b1b425082ed47bcbc71a870d8889d259e089b96c
Raitis-Silins <raiti.silins@gmail.com>
2021-12-14 20:50:20 +0200

    Exercise9 complete

---
+++ csharp-basics/exercises/Collections/Exercise9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exercise9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var myText = new HashSet<string>();
+
+            myText.Add("Flight");
+            myText.Add("planner");
+            myText.Add("was");
+            myText.Add("really");
+            myText.Add("hard");
+
+            foreach (var text in myText)
+            {
+                Console.WriteLine(text);
+            }
+
+            myText.Clear();
+
+            Console.WriteLine();
+
+            myText.Add("Flight");
+            myText.Add("Flight");
+
+            foreach (var text in myText)
+            {
+                Console.WriteLine(text);
+            }
+
+        }
+    }
+}
