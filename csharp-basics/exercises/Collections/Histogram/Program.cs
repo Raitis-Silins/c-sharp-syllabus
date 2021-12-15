5a99adb6c6d6363bc7313e3b6f5d947a48410ab2
Raitis-Silins <raiti.silins@gmail.com>
2021-12-16 01:15:04 +0200

    Exercise7 complete

--- csharp-basics/exercises/Collections/Histogram/Program.cs
+++ csharp-basics/exercises/Collections/Histogram/Program.cs
@@ -1,22 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Histogram
 {
     class Program
     {
-        private const string Path = "../Histogram/midtermscores.txt";
+        private const string Path = "../../midtermscores.txt";
 
-        private static void Main(string[] args)
+        private static void Main()
         {
-            var readText = File.ReadAllLines(Path);
-            foreach (var s in readText)
+            const int points = 10;
+            var readText = File.ReadAllText(Path);
+            var scores = readText.Split(' ');
+            List<string> gradeList = new List<string>();
+
+            string[] grade = {"00-09:","10-19:","20-29:","30-39:", "40-49:", "50-59:", "60-69:","70-79:","80-89:"
+                ,"90-99:"," 100:"};
+            gradeList.AddRange(grade);
+
+            foreach (var value in scores)
             {
-                Console.WriteLine(s);
+                var num = int.Parse(value) / points;
+                grade[num] += "*";
+            }
+
+            for (int i = 0; i < gradeList.Count; i++)
+            {
+                string result = grade[i];
+                Console.WriteLine(result);
             }
         }
     }
