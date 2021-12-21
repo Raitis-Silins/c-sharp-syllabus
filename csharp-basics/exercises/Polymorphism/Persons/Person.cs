de39c4a5dca6bfb2baed266b85ddf6383e20e7bb
Raitis-Silins <raiti.silins@gmail.com>
2021-12-22 00:27:26 +0200

    Exercise3 complete

---
+++ csharp-basics/exercises/Polymorphism/Persons/Person.cs
@@ -0,0 +1,20 @@
+﻿namespace Hierarchy
+{
+    abstract class Person
+    {
+        protected string _firstName;
+        protected string _lastName;
+        protected string _address;
+        protected int _id;
+
+        public Person(string firstName, string lastName, string address, int id)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _address = address;
+            _id = id;
+        }
+
+        public abstract void Display();
+    }
+}
