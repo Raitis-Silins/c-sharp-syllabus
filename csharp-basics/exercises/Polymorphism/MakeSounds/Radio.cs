8b4e8620e7ae8c8371e39619d25ca4f057e93c1e
Raitis-Silins <raiti.silins@gmail.com>
2021-12-21 03:52:24 +0200

    Exercise 2 complete

---
+++ csharp-basics/exercises/Polymorphism/MakeSounds/Radio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MakeSounds
+{
+    class Radio: ISound
+    {
+        public void PlaySound()
+        {
+            Console.WriteLine("electromagnetic atmospheric noise");
+        }
+    }
+}
