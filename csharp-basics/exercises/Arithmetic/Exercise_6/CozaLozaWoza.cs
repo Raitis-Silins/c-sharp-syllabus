360059b272478644a50b64edab25feb869111fcf
Raitis-Silins <raiti.silins@gmail.com>
2022-01-02 20:19:31 +0200

    Arithmetics Exercise_6 Tests changes/ completed

--- csharp-basics/exercises/Arithmetic/Exercise_6/CozaLozaWoza.cs
+++ csharp-basics/exercises/Arithmetic/Exercise_6/CozaLozaWoza.cs
@@ -36,5 +36,47 @@ namespace Exercise_6
                 return number + " ";
             }
         }
+
+        public string CozaLozaMain()
+        {
+            for (int i = 1; i <= 110; i++)
+            {
+                if (i % 5 == 0 && i % 7 == 0)
+                {
+                    Console.Write("LozaWoza ");
+                }
+                else if (i % 3 == 0 && i % 7 == 0)
+                {
+                    Console.Write("CozaWoza ");
+                }
+                else if (i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.Write("CozaLoza ");
+                }
+                else if (i % 7 == 0)
+                {
+                    Console.Write("Woza ");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write("Loza ");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write("Coza ");
+                }
+                else
+                {
+                    Console.Write(i + " ");
+                }
+
+                if (i % 11 == 0)
+                {
+                    Console.WriteLine("\n");
+                }
+            }
+
+            return null;
+        }
     }
 }
