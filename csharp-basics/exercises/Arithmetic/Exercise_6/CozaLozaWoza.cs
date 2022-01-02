621bb13e8f333bf1afc20dc319b3714443c8bbde
Raitis-Silins <raiti.silins@gmail.com>
2022-01-02 20:53:40 +0200

    Exercise_6 final changes

--- csharp-basics/exercises/Arithmetic/Exercise_6/CozaLozaWoza.cs
+++ csharp-basics/exercises/Arithmetic/Exercise_6/CozaLozaWoza.cs
@@ -11,31 +11,40 @@ namespace Exercise_6
             {
                 return "LozaWoza ";
             }
-            else if (number % 3 == 0 && number % 7 == 0)
+
+            if (number % 3 == 0 && number % 7 == 0)
             {
                 return "CozaWoza ";
             }
-            else if (number % 3 == 0 && number % 5 == 0)
+
+            if (number % 3 == 0 && number % 5 == 0)
             {
                 return "CozaLoza ";
             }
-            else if (number % 7 == 0)
+
+            if (number % 7 == 0)
             {
                 return "Woza ";
             }
-            else if (number % 5 == 0)
+
+            if (number % 5 == 0)
             {
                 return "Loza ";
             }
-            else if (number % 3 == 0)
+
+            if (number % 3 == 0)
             {
                 return "Coza ";
             }
-            else
+
+            if (number % 5 != 0 && number % 7 != 0 && number % 3 != 0)
             {
                 return number + " ";
             }
+
+            return null;
         }
+        
 
         public string CozaLozaMain()
         {
