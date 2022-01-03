0cd9d896a5e4afb95d3a717579db4ab5f48df223
Raitis-Silins <raiti.silins@gmail.com>
2022-01-04 00:50:56 +0200

    Collections Exercise 5 Tests Done

---
+++ csharp-basics/exercises/Collections/WordCount.Tests/CalculateTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WordCount.Tests
+{
+    [TestClass]
+    public class CalculateTests
+    {
+        private Calculate _target;
+
+        [TestMethod]
+        public void CalculateLinesInText_InputWith2Lines_Expected2()
+        {
+            //Arrange
+            var text = "My name is Raitis \n and I live in Tokyo.";
+            var expected = 2;
+            _target = new Calculate(text);
+
+            //Act
+            var result = _target.LinesInText();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void CalculateWordsInText_InputWith4Words_Expected4()
+        {
+            //Arrange
+            var text = "My name is Raitis";
+            var expected = 4;
+            _target = new Calculate(text);
+
+            //Act
+            var result = _target.WordsInText();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void CalculateCharsInText_InputWith17Chars_Expected17()
+        {
+            //Arrange
+            var text = "I want my chicken";
+            var expected = 17;
+            _target = new Calculate(text);
+
+            //Act
+            var result = _target.CharsInText();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
---
+++ csharp-basics/exercises/Collections/WordCount/Calculate.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.RegularExpressions;
+
+namespace WordCount
+{
+    public class Calculate
+    {
+        private string _text;
+
+        public Calculate(string text)
+        {
+            _text = text;
+        }
+
+        public int LinesInText()
+        {
+            return _text.Split('\n').Length;
+        }
+
+        public int WordsInText()
+        {
+            return Regex.Matches(_text, @"[A-Za-z]+").Count;
+        }
+
+        public int CharsInText()
+        {
+            return _text.Replace("\n", "").ToCharArray().Length;
+        }
+    }
+}
