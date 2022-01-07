7f41b9b4ad886878725a71761b2f820654164433
Raitis-Silins <raiti.silins@gmail.com>
2022-01-07 23:26:47 +0200

    Arithmetics Exercise_8 Tests Changes/Done

--- csharp-basics/exercises/Arithmetic/Exercise_8/Salary.cs
+++ csharp-basics/exercises/Arithmetic/Exercise_8/Salary.cs
@@ -4,21 +4,11 @@ public class Salary
 {
     public double DetermineSalary(double basePay, double hoursWorked)
     {
-        if (basePay < 8)
+        if (basePay < 8 || hoursWorked > 60)
         {
             throw new Exception("error");
         }
 
-        if (hoursWorked > 60)
-        {
-            throw new Exception("error");
-        }
-
-        if (hoursWorked > 40)
-        {
-            return Math.Round(basePay * 40 + (double)1.5 * basePay * (hoursWorked - 40), 2);
-        }
-
-        return Math.Round(basePay * hoursWorked, 2);
+        return hoursWorked > 40 ? Math.Round(basePay * 40 + (double)1.5 * basePay * (hoursWorked - 40), 2) : Math.Round(basePay * hoursWorked, 2);
     }
 }
