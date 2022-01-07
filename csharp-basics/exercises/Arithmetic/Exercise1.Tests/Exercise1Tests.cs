f4017dce0518b3a36b32081fbcb39e358f53472a
Raitis-Silins <raiti.silins@gmail.com>
2022-01-08 01:25:53 +0200

    Arithmetics Exercise_1 Tests Changes/Done

--- csharp-basics/exercises/Arithmetic/Exercise1.Tests/Exercise1Tests.cs
+++ csharp-basics/exercises/Arithmetic/Exercise1.Tests/Exercise1Tests.cs
@@ -14,62 +14,18 @@ namespace Exercise1.Tests
             _target = new Check15();
         }
 
-        [TestMethod]
-        public void CheckIntegers_Input63and43_ShouldReturnFalse()
+        [DataTestMethod]
+        [DataRow(10, 5)]
+        [DataRow(15, 43)]
+        [DataRow(31, 16)]
+        [DataRow(10, 25)]
+        public void CheckIntegers_If15_ShouldReturnTrue(int a, int b)
         {
-            //Arrange
-            var num1 = 63;
-            var num2 = 43;
-
             //Act
-            var result = _target.CheckIntegers(num1, num2);
-
-            //Assert
-            Assert.AreEqual(false, result);
-        }
-
-        [TestMethod]
-        public void CheckIntegers_Input15and43_ShouldReturnTrue()
-        {
-            //Arrange
-            var num1 = 15;
-            var num2 = 43;
-
-            //Act
-            var result = _target.CheckIntegers(num1, num2);
+            var result = _target.CheckIntegers(a, b);
 
             //Assert
             Assert.AreEqual(true, result);
-        }
-
-        [TestMethod]
-        public void CheckIntegers_IfSumIs15_ShouldReturnTrue()
-        {
-            //Arrange
-            var num1 = 10;
-            var num2 = 5;
-
-            //Act
-            var result = _target.CheckIntegers(num1, num2);
-
-            //Assert
-            Assert.AreEqual(true, result);
-        }
-
-        [TestMethod]
-        public void CheckIntegers_IfSubtractionIs15_ShouldReturnTrue()
-        {
-            //Arrange
-            var num1 = 31;
-            var num2 = 16;
-
-            //Act
-            var result1 = _target.CheckIntegers(num1, num2);
-            var result2 = _target.CheckIntegers(num2, num1);
-
-            //Assert
-            Assert.AreEqual(true, result1);
-            Assert.AreEqual(true, result2);
         }
     }
 }
